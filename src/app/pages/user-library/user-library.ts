d2cc732abb63be7716505aee44531f31d51a6ac2
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-08 14:48:30 +0100

    feat: add initial page components and templates for auth, details, home, search, and user library

---
+++ src/app/pages/user-library/user-library.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-library',
+  imports: [],
+  templateUrl: './user-library.html',
+  styleUrl: './user-library.scss',
+})
+export class UserLibrary {
+
+}
