93671b66fb221c23f839c2742c25def13b2c8a4f
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-08 16:30:00 +0100

    feat: fetch new releases to home component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { Spotify } from '../../services/spotify';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  private spotifyService = inject(Spotify);
 
+  private allNewReleases = toSignal(this.spotifyService.getNewReleases());
+
+  featuredNewReleases = computed(() => {
+    const data = this.allNewReleases();
+    if (!data) return [];
+
+    return data.items.filter((item) => item.type === 'album').slice(0, 10);
+  });
 }
