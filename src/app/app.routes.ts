db1f46db99296ecc88a653c1b70a575611d7bdb9
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-08 14:49:02 +0100

    feat: define routes for all pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Search } from './pages/search/search';
+import { Details } from './pages/details/details';
+import { Auth } from './pages/auth/auth';
+import { UserLibrary } from './pages/user-library/user-library';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'search', component: Search },
+  { path: 'details/:type/:id', component: Details },
+  { path: 'auth', component: Auth },
+  { path: 'user-library', component: UserLibrary },
+  { path: '**', redirectTo: '' },
+];
