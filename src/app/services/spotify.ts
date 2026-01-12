759fa0772ffcf7880f89568f2986a013a57e05c1
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-12 13:16:39 +0100

    feat: add methods to fetch artist, album, track, and artist albums

--- src/app/services/spotify.ts
+++ src/app/services/spotify.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { AlbumListResponse } from '../models/music';
+import { Album, AlbumListResponse, Artist, Track } from '../models/music';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +11,21 @@ export class Spotify {
 
   getNewReleases() {
     return this.http.get<AlbumListResponse>(`${this.baseUrl}/new-releases`);
+  }
+
+  getArtist(id: string) {
+    return this.http.get<Artist>(`${this.baseUrl}/artists/${id}`);
+  }
+
+  getAlbum(id: string) {
+    return this.http.get<Album>(`${this.baseUrl}/albums/${id}`);
+  }
+
+  getTrack(id: string) {
+    return this.http.get<Track>(`${this.baseUrl}/tracks/${id}`);
+  }
+
+  getArtistAlbums(id: string) {
+    return this.http.get<Album[]>(`${this.baseUrl}/artists/${id}/albums`);
   }
 }
