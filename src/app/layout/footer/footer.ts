a207fe8ff841c8eaa37ce300ebcb2ef33992a2e4
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-16 15:48:11 +0100

    feat: add footer content with copyright and social links

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
+  readonly name = 'Frida Nordenlöw';
+  readonly currentYear = new Date().getFullYear();
+  readonly title = 'Front End Developer';
+  readonly school = 'Medieinstitutet';
 
+  readonly githubUrl = 'https://github.com/fridanordenlow';
+  readonly linkedInUrl = 'https://linkedin.com/in/fridanordenlow';
 }
