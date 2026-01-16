8f6554ec1f761bf66e74bae9f6887727bcaefc3d
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-16 15:47:31 +0100

    feat: update header layout with responsive mobile menu and angular material components

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDivider } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    RouterLink,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDivider,
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
-
-}
+export class Header {}
