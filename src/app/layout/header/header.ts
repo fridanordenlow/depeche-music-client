0c821bbc18967e9a3c420e428c762fa8c8976545
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-23 14:04:27 +0100

    refactor: move search bar into it's own component and refactor header

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,67 +1,24 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, inject } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDivider } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { debounceTime, distinctUntilChanged, filter, take } from 'rxjs';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-header',
-  imports: [
-    ReactiveFormsModule,
-    RouterLink,
-    MatButtonModule,
-    MatDivider,
-    MatFormField,
-    MatIconModule,
-    MatInputModule,
-    MatLabel,
-    MatMenuModule,
-    MatToolbarModule,
-  ],
+  imports: [RouterLink, MatButtonModule, MatDivider, MatIconModule, MatMenuModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
   private readonly router = inject(Router);
-  // private destroyRef = inject(DestroyRef);
   public auth = inject(AuthService);
 
-  searchControl = new FormControl('');
-
-  constructor() {
-    this.searchControl.valueChanges
-      .pipe(
-        debounceTime(500),
-        distinctUntilChanged(),
-        filter((query): query is string => !!query && query.length > 2),
-        takeUntilDestroyed()
-      )
-      .subscribe((query) => {
-        this.router.navigate(['/search'], { queryParams: { q: query } });
-      });
-
-    this.router.events
-      .pipe(
-        filter((event) => event instanceof NavigationEnd),
-        takeUntilDestroyed()
-      )
-      .subscribe(() => {
-        this.searchControl.setValue('', { emitEvent: false });
-      });
-  }
-
   onLogin() {
-    this.router.navigate(['/auth']);
-    // Todo: Add redirect to current page after login
+    this.router.navigate(['/auth'], { queryParams: { returnUrl: this.router.url } });
   }
 
   onLogout() {
