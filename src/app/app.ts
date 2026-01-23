2e21c47e92cf5ef19f52679c15e708c9f12e3e68
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-23 14:06:48 +0100

    feat: add search bar component to app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
 import { AuthService } from './services/auth';
 import { LibraryService } from './services/library';
+import { SearchBar } from './shared/search-bar/search-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, SearchBar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
