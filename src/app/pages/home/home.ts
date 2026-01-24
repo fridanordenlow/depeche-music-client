29dbb296065072e13d4eb0d3ecf957da7e8ed136
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-24 19:42:50 +0100

    refactor/style: move new-releases to it's own file and update the styling

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,29 +1,11 @@
-import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
-import { SpotifyService } from '../../services/spotify';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { RouterLink } from '@angular/router';
-import { Album } from '../../models/music';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NewReleases } from '../../components/new-releases/new-releases';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [NewReleases],
   templateUrl: './home.html',
   styleUrl: './home.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Home {
-  private spotifyService = inject(SpotifyService);
-
-  private allNewReleases = toSignal(this.spotifyService.getNewReleases());
-
-  featuredNewReleases = computed(() => {
-    const data = this.allNewReleases();
-    if (!data) return [];
-
-    return data.items.filter((item) => item.type === 'album').slice(0, 10);
-  });
-
-  getArtistNames(album: Album): string {
-    return album.artists.map((a) => a.name).join(', ');
-  }
-}
+export class Home {}
