4829670084b5e53107ec1e1b3841f27908eb2dd0
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-27 14:37:32 +0100

    feat: add UserReleasesRecommendations component to home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NewReleases } from '../../components/new-releases/new-releases';
+import { UserReleasesRecommendations } from '../../components/user-releases-recommendations/user-releases-recommendations';
 
 @Component({
   selector: 'app-home',
-  imports: [NewReleases],
+  imports: [NewReleases, UserReleasesRecommendations],
   templateUrl: './home.html',
   styleUrl: './home.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
