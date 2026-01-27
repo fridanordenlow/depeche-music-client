5a57fa66085b14b73cfafd7967804e65897330a5
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-27 14:37:14 +0100

    feat: add UserReleasesRecommendations component for displaying user album recommendations

---
+++ src/app/components/user-releases-recommendations/user-releases-recommendations.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { RecommendationService } from '../../services/recommendation';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Loading } from '../../shared/loading/loading';
+
+@Component({
+  selector: 'app-user-releases-recommendations',
+  imports: [CommonModule, RouterLink, Loading],
+  templateUrl: './user-releases-recommendations.html',
+  styleUrl: './user-releases-recommendations.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UserReleasesRecommendations {
+  private recommendationService = inject(RecommendationService);
+
+  private allRecommendations = toSignal(this.recommendationService.getPublicRecommendations(), {
+    initialValue: [],
+  });
+
+  isLoading = computed(() => !this.allRecommendations());
+
+  albumRecommendations = computed(() => {
+    return this.allRecommendations().filter((rec) => rec.type === 'album');
+  });
+}
