a58b50a289bbf604b2596d957c63a369a7b13840
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2026-01-28 10:42:09 +0100

    fix: update UserRecommendation interface

--- src/app/models/recommendation.ts
+++ src/app/models/recommendation.ts
@@ -1,15 +1,20 @@
 export interface UserRecommendation {
   _id: string;
-  userId: string;
+  user: {
+    _id: string;
+    username: string;
+  };
   spotifyId: string;
   type: 'album' | 'artist' | 'track';
   review: string;
   isFeatured: boolean;
   createdAt: Date;
   metadata?: {
+    spotifyId: string;
+    type: string;
     title?: string;
     name?: string;
-    imageUrl?: string;
-    artists?: { name: string }[];
+    imageUrl: string;
+    artists?: { id: string; name: string }[];
   };
 }
